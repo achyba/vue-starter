e8be3d015201cb19e1793604d39790abf2d24e31
agorska <agorska@d17-430-pc-03.ipa.ii.agh.edu.pl>
2023-04-16 11:07:34 +0200

    pierwsza polowa zajec

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
+
+    <div v-if="logged">{{email}} jesteś zalogowany
+      <button @click="logOut()">Wyloguj</button>
+    </div>
+
+
+    <div v-else>
+    Witaj w systemie do zapisów na zajęcia ^.^
+    Zaloguj się e-mailem
+
+    <input type="email" v-model="email">
+    <button @click="logIn()">Zaloguj</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
 
+export default {
+  data() { //stan  komponentu - data go zwraca
+    return {
+      email: '',
+      password: '',
+      logged: false,
+    };
+  },
+  methods: {
+    logIn() {
+      this.logged=true;
+    },
+    logOut() {
+      this.logged=false;
+    }
+  }
 }
 </script>
 
@@ -14,6 +41,21 @@ export default {
 .my-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   text-align: center;
-  color: #2c3e50;
+  color: deeppink;
+}
+.my-app div{
+  margin-top: 20px;
+}
+button{
+  margin-left: 2px;
+  background-color: pink;
+  border: none;
+}
+
+button:hover{
+  background-color: deeppink;
+  border: none;
+  border-radius: 12px;
+  transition-duration: 0.4s;
 }
 </style>
